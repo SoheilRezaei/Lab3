4bd76cb7ca6626d1e6cf4dc86ced391d69b0d64b
SoheilRezaei <soheilrezaei966@gmail.com>
2022-11-06 12:50:02 -0500

    Added the Search by Track and Album Requester in javascript

--- static/script.js
+++ static/script.js
@@ -25,3 +25,16 @@ async function SearchByArtist(artist){
         }
     })
 }
+
+// Adding the SEarch by Track or Album request
+
+async function SearchByTrack(track){
+    fetch("http://localhost:5000/TrckAl/" + track)
+    .then((res) => res.json())
+    .then((data) => {
+        var i = Object.keys(data).length;
+        for(var i = 0 ; i < Object.keys(data).length ; i++ ){
+        console.log(data[i]);
+        }
+    })
+}
