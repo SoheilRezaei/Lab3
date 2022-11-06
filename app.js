e7b81ab5d9f082b1a62840893bb66db6d95c0fa1
SoheilRezaei <soheilrezaei966@gmail.com>
2022-11-06 11:08:04 -0500

    Added the Genre Request handler to node.js

--- app.js
+++ app.js
@@ -1,28 +1,57 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const router = express.Router();
 
 
 const fs = require("fs");
 const { parse } = require("csv-parse");
 
+var Genre = [];
+var Artist = [];
+var trackAlbum = [];
+let result = [];
 
-var genre = [];
-
+// Creating the back end handler for Search by Genre request
 
 fs.createReadStream("./lab3-data/genres.csv")
   .pipe(parse({ delimiter: ",", from_line: 2 }))
   .on("data", function (row) {
-    genre.push(row);
+    Genre.push(row);
   })
   .on("end", function () {
-    console.log("finished");
-    console.log(genre[10]);
+    console.log("finished reading genre");
   })
   .on("error", function (error) {
     console.log(error.message);
   });
   
+  function searchByGenre(genreName) {
+    for (let i=0; i<Genre.length; i++){
+      var phrase = new RegExp(genreName, "gi");
+      let Result = Genre[i][3].match(phrase);
+      if (Result != null){
+        console.log(Genre[i][3]);
+        for (let j=0; j<Genre.length; j++){
+          if (Genre[i][4] == Genre[j][0]){
+            console.log(" SubGenre of : " + Genre[j][3]);
+            result.push([Genre[i][3], Genre[j][3]]);
+          }
+        }
+      }
+    }
+    console.log(result)
+  }
+
+  app.get ('/Genre/:name', (req, res) =>{
+    searchByGenre(req.params.name);
+    res.send(result);
+    result.length = 0;
+  });
+
+
+
+
+
 
 app.use('/', express.static('static'));
 
@@ -30,7 +59,7 @@ app.use('/', express.static('static'));
 //     console.log(`GET request for ${req.url}`);
 //     res.send(genre);
 // });
-
+var port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
